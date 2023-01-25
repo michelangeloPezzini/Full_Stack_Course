cad40258b68f8e6239bddac45f9e4c0ac84b4285
Michelangelo Pezzini <michelangelo.pezzini@compasso.com.br>
2023-01-25 11:00:11 -0300

    update

---
+++ javaScript/Funcoes/script.js
@@ -0,0 +1,28 @@
+//Parametro é uma variavel que fica disponivel dentro da função
+//Ela é prenchida ou informada quando a função é chamada
+
+//Os parametros podem ter um valor default, que seria um valor padrão
+function nome(nome = "Mike") {
+  console.log(`Olá ${nome}`);
+}
+nome("Gabriela");
+
+function soma(a, b) {
+  const resultado = a + b;
+  return resultado;
+}
+
+const novaSoma = soma(5, 8);
+console.log(novaSoma);
+
+function criarProduto(nome, preco) {
+  const produto = {
+    nome,
+    preco,
+    estoque: 1,
+  };
+  return produto;
+}
+
+const notebook = criarProduto("Nitro 5 Core i5 16 gb", 4500);
+console.log(notebook);
