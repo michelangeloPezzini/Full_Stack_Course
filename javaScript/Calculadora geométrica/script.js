2491128536302de3d3a31cf77c0305bdb3c2f205
Michelangelo Pezzini <michelangelo.pezzini@compasso.com.br>
2023-01-30 15:28:30 -0300

    update

--- javaScript/Calculadora geométrica/script.js
+++ javaScript/Calculadora geométrica/script.js
@@ -8,20 +8,69 @@ function areaTriangulo(base, altura) {
   alert(`Resultado = ${resultado}`);
 }
 
+function areaRetangulo(base, altura) {
+  const resultado = base * altura;
+  alert(`Resultado = ${resultado}`);
+}
+
+function areaQuadrado(lado) {
+  const resultado = lado * lado;
+  alert(`Resultado = ${resultado}`);
+}
+
+function areaTrapezio(baseMaior, baseMenor, altura) {
+  const resultado = ((baseMaior + baseMenor) * altura) / 2;
+  alert(`Resultado = ${resultado}`);
+}
+
+function areaCirculo(raio) {
+  const pi = 3.14;
+  const resultado = pi * (raio * raio);
+  alert(`Resultado = ${resultado}`);
+}
+
 do {
   opcao = prompt(`
   Escolha uma opção:
-  1: Área do triângulo
-  2: Área do retângulo
-  3: Área do quadrado
-  4: Área do trapézio
-  5: Área do circulo`);
+  1: Área do Triângulo
+  2: Área do Retângulo
+  3: Área do Quadrado
+  4: Área do Trapézio
+  5: Área do Círculo
+  6: Sair`);
   switch (opcao) {
     case "1":
-      const base = prompt("Qual a base do Triagulo? ");
-      const altura = prompt("Qual a altura do Triagulo? ");
-      areaTriangulo(base, altura);
+      const baseTriangulo = prompt("Qual a base do Triagulo? ");
+      const alturaTriangulo = prompt("Qual a altura do Triagulo? ");
+      areaTriangulo(baseTriangulo, alturaTriangulo);
       break;
+
+    case "2":
+      const baseRetangulo = prompt("Qual a é a base do Retângulo??");
+      const alturaRetangulo = prompt("Qual a é a altura do Retângulo??");
+      areaRetangulo(baseRetangulo, alturaRetangulo);
+      break;
+
+    case "3":
+      const ladoQuadrado = prompt("Qual o lado do Quadrado?");
+      areaQuadrado(ladoQuadrado);
+      break;
+
+    case "4":
+      const baseMaiorTrapezio = prompt("Qual a Base Maior do Trapézio?");
+      const baseMenorTrapezio = prompt("Qual a Base Menor do Trapézio?");
+      const alturaTrapezio = prompt("Qual a altura do Trapézio?");
+      areaTrapezio(baseMaiorTrapezio, baseMenorTrapezio, alturaTrapezio);
+      break;
+
+    case "5":
+      const raioCirculo = prompt("Qual o Raio do círculo?");
+      areaCirculo(raioCirculo);
+      break;
+
+    case "6":
+      alert("Encerrando...");
+
     default:
       alert("Digite novamente");
   }
